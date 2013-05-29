f3b7548008f9334e5daddabe2956a2da4fed839b
Nick <nhnatiw@gmail.com>
2013-05-29 16:35:12 -0300

    Added fields
    All the fields should be about done.
    I still need to figure out what the boolean field method name is for django

--- feeds/models.py
+++ feeds/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Feeds(models.model):
-    url = models.charfield(max_length=500)
+    # these fields should function well enough for RSS and Atom.  I still need to look at how podcasts work
+    url = models.charfield(max_length=2000)
     version = models.charfield(max_length=5)
-    title = models.charfield(max_length=200)
-    description = models.charfield(max_length=500)
+    title = models.charfield(max_length=500)
+    description = models.charfield(max_length=2000)
     
 class article(models.model):
     feed = models.ForiegnKey(Feeds)
-    link models.charfield(max_length=500)
+    link = models.charfield(max_length=2000)
     title = models.charfield(max_length=500)
-    description = models.charfield(max_length=500)
+    description = models.charfield(max_length=2000)
+    viewed = models.
+    
+class subscriptions(models.model):
+    user_id = models.ForiegnKey(User)
+    feed_id = models.ForiegnKey(Feeds)
