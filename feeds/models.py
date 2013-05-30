dee81e0cc65bc458916495e7cae6fb4d416baea9
Nick <nhnatiw@gmail.com>
2013-05-30 15:07:18 -0300

    Update models.py
    Fixed some errors
    added the boolean field for the articles table

--- feeds/models.py
+++ feeds/models.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 class Feeds(models.model):
     # these fields should function well enough for RSS and Atom.  I still need to look at how podcasts work
     url = models.charfield(max_length=2000)
-    version = models.charfield(max_length=5)
-    title = models.charfield(max_length=500)
-    description = models.charfield(max_length=2000)
+    version = models.CharField(max_length=5)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
     
 class article(models.model):
     feed = models.ForiegnKey(Feeds)
-    link = models.charfield(max_length=2000)
-    title = models.charfield(max_length=500)
-    description = models.charfield(max_length=2000)
-    viewed = models.
+    link = models.CharField(max_length=2000)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
+    viewed = models.BooleanField()
     
 class subscriptions(models.model):
     user_id = models.ForiegnKey(User)
